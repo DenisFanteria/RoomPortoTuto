26eda77d54705cd0c62d052007c6774e542b6928
DenisFanteria <67110836+DenisFanteria@users.noreply.github.com>
2022-12-22 19:00:13 +0100

    html work

--- Experience/World/Controls.js
+++ Experience/World/Controls.js
@@ -1,5 +1,6 @@
 import * as THREE from "three"
 import GSAP from "gsap"
+import { ScrollTrigger } from "gsap/all"
 import Experience from "../Experience"
 
 export default class Controls {
@@ -9,6 +10,24 @@ export default class Controls {
     this.resources = this.experience.resources
     this.time = this.experience.time
     this.camera = this.experience.camera
+    this.room = this.experience.world.room.actualRoom
+    GSAP.registerPlugin(ScrollTrigger)
+
+    this.setPath()
+  }
+
+  setPath() {
+    this.timeline = new GSAP.timeline()
+    this.timeline.to(this.room.position, {
+      x: 5,
+      scrollTrigger: {
+        trigger: ".first-move",
+        markers: true,
+        start: "top top",
+        end: "bottom bottom",
+        scrub: 0.5
+      }
+    })
   }
 
   resize() {}
