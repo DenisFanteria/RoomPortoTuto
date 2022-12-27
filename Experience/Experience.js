43e6a1d911cc17b9ae2f55516245790ece0e3d45
DenisFanteria <67110836+DenisFanteria@users.noreply.github.com>
2022-12-27 20:35:47 +0100

    Theme work

--- Experience/Experience.js
+++ Experience/Experience.js
@@ -1,49 +1,48 @@
-import * as THREE from 'three'
+import * as THREE from "three"
 
-import Sizes from './Utils/Sizes'
-import Time from './Utils/Time'
-import Resources from './Utils/Resources'
-import assets from './Utils/assets'
+import Sizes from "./Utils/Sizes"
+import Time from "./Utils/Time"
+import Resources from "./Utils/Resources"
+import assets from "./Utils/assets"
 
-import Camera from './Camera'
-import Renderer from './Renderer'
+import Camera from "./Camera"
+import Renderer from "./Renderer"
+import Theme from "./Theme"
 
-import World from './World/World'
-
+import World from "./World/World"
 
 export default class Experience {
-    static instance
-    constructor(canvas) {
-        if (Experience.instance) {
-            return Experience.instance
-        }
-        Experience.instance = this
-        this.canvas = canvas
-        this.scene = new THREE.Scene()
-        this.time = new Time()
-        this.sizes = new Sizes()
-        this.camera = new Camera()
-        this.renderer = new Renderer()
-        this.resources = new Resources(assets)
-        this.world = new World()
-
-        this.sizes.on("resize", () => {
-            this.resize()
-        })
-        this.time.on("update", () => {
-            this.update()
-        })
-
+  static instance
+  constructor(canvas) {
+    if (Experience.instance) {
+      return Experience.instance
     }
-    resize() {
-        this.camera.resize()
-        this.world.resize()
-        this.renderer.resize()
-    }
-    update() {
-        this.camera.update()
-        this.world.update()
-        this.renderer.update()
-    }
+    Experience.instance = this
+    this.canvas = canvas
+    this.scene = new THREE.Scene()
+    this.time = new Time()
+    this.sizes = new Sizes()
+    this.camera = new Camera()
+    this.renderer = new Renderer()
+    this.resources = new Resources(assets)
+    this.theme = new Theme()
+    this.world = new World()
 
+    this.sizes.on("resize", () => {
+      this.resize()
+    })
+    this.time.on("update", () => {
+      this.update()
+    })
+  }
+  resize() {
+    this.camera.resize()
+    this.world.resize()
+    this.renderer.resize()
+  }
+  update() {
+    this.camera.update()
+    this.world.update()
+    this.renderer.update()
+  }
 }
