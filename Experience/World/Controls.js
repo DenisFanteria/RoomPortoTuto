4956be9dad3a3930e5e9d8078e64b3de5eb80b08
DenisFanteria <67110836+DenisFanteria@users.noreply.github.com>
2023-01-08 18:56:43 +0100

    Mini floor animation

--- Experience/World/Controls.js
+++ Experience/World/Controls.js
@@ -26,6 +26,11 @@ export default class Controls {
     ScrollTrigger.matchMedia({
       // Desktop
       "(min-width: 969px)": () => {
+        // Resets
+        this.room.scale.set(0.11, 0.11, 0.11)
+        this.rectLight.width = 0.5
+        this.rectLight.height = 0.7
+
         // First Section ---------------------------
         this.firstMoveTimeline = new GSAP.timeline({
           scrollTrigger: {
@@ -97,11 +102,178 @@ export default class Controls {
         })
       },
       // Mobile
-      "(max-width: 968px)": () => {},
+      "(max-width: 968px)": () => {
+        // Resets
+
+        this.room.scale.set(0.07, 0.07, 0.07)
+        this.room.position.set(0, 0, 0)
+        this.rectLight.width = 0.3
+        this.rectLight.height = 0.4
+
+        // First Section ---------------------------
+        this.firstMoveTimeline = new GSAP.timeline({
+          scrollTrigger: {
+            trigger: ".first-move",
+            top: "top top",
+            bottom: "bottom bottom",
+            scrub: 0.6,
+            invalidateOnRefresh: true
+          }
+        }).to(this.room.scale, {
+          x: 0.1,
+          y: 0.1,
+          z: 0.1
+        })
+        // Second Section ---------------------------
+        this.secondMoveTimeline = new GSAP.timeline({
+          scrollTrigger: {
+            trigger: ".second-move",
+            top: "top top",
+            bottom: "bottom bottom",
+            scrub: 0.6,
+            invalidateOnRefresh: true
+          }
+        })
+          .to(
+            this.room.scale,
+            {
+              x: 0.25,
+              y: 0.25,
+              z: 0.25
+            },
+            "same"
+          )
+          .to(
+            this.rectLight,
+            {
+              width: 0.3 * 3.4,
+              height: 0.4 * 3.4
+            },
+            "same"
+          )
+          .to(
+            this.room.position,
+            {
+              x: 1.5
+            },
+            "same"
+          )
+
+        // Third Section ---------------------------
+        this.thirdMoveTimeline = new GSAP.timeline({
+          scrollTrigger: {
+            trigger: ".third-move",
+            top: "top top",
+            bottom: "bottom bottom",
+            scrub: 0.6,
+            invalidateOnRefresh: true
+          }
+        }).to(this.room.position, {
+          z: -4.5
+        })
+      },
 
       // all
-      all: function () {}
+      all: () => {
+        // Mini Platform animation
+        this.secondPartTimeline = new GSAP.timeline({
+          scrollTrigger: {
+            trigger: ".third-move",
+            start: "center center"
+          }
+        })
+
+        this.room.children.forEach(child => {
+          if (child.name === "Mini_Floor") {
+            this.first = GSAP.to(child.position, {
+              x: -5.44055,
+              z: 13.6135,
+              duration: 0.3
+            })
+          }
+          if (child.name === "Mailbox") {
+            this.second = GSAP.to(child.scale, {
+              x: 1,
+              y: 1,
+              z: 1,
+              duration: 0.3
+            })
+          }
+          if (child.name === "Lamp") {
+            this.third = GSAP.to(child.scale, {
+              x: 1,
+              y: 1,
+              z: 1,
+              ease: "back.out(2)",
+              duration: 0.3
+            })
+          }
+          if (child.name === "FloorFirst") {
+            this.fourth = GSAP.to(child.scale, {
+              x: 1,
+              y: 1,
+              z: 1,
+              ease: "back.out(2)",
+              duration: 0.3
+            })
+          }
+          if (child.name === "FloorSecond") {
+            this.fifth = GSAP.to(child.scale, {
+              x: 1,
+              y: 1,
+              z: 1,
+              ease: "back.out(2)",
+              duration: 0.3
+            })
+          }
+          if (child.name === "FloorThird") {
+            this.sixth = GSAP.to(child.scale, {
+              x: 1,
+              y: 1,
+              z: 1,
+              ease: "back.out(2)",
+              duration: 0.3
+            })
+          }
+          if (child.name === "Dirt") {
+            this.seventh = GSAP.to(child.scale, {
+              x: 1,
+              y: 1,
+              z: 1,
+              ease: "back.out(2)",
+              duration: 0.3
+            })
+          }
+          if (child.name === "Flower1") {
+            this.eighth = GSAP.to(child.scale, {
+              x: 1,
+              y: 1,
+              z: 1,
+              ease: "back.out(2)",
+              duration: 0.3
+            })
+          }
+          if (child.name === "Flower2") {
+            this.ninth = GSAP.to(child.scale, {
+              x: 1,
+              y: 1,
+              z: 1,
+              ease: "back.out(2)",
+              duration: 0.3
+            })
+          }
+        })
+      }
     })
+    this.secondPartTimeline.add(this.first)
+    this.secondPartTimeline.add(this.second)
+    this.secondPartTimeline.add(this.third)
+    this.secondPartTimeline.add(this.fourth, "-=0.2")
+    this.secondPartTimeline.add(this.fifth, "-=0.2")
+    this.secondPartTimeline.add(this.sixth, "-=0.2")
+    this.secondPartTimeline.add(this.seventh, "-=0.2")
+    this.secondPartTimeline.add(this.eighth)
+    this.secondPartTimeline.add(this.ninth, "-=0.1")
   }
 
   resize() {}
