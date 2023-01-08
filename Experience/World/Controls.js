c4fbbcbf422ca731a9bd396360b1ead41b760aba
DenisFanteria <67110836+DenisFanteria@users.noreply.github.com>
2023-01-08 21:08:27 +0100

    Progress Bar Work

--- Experience/World/Controls.js
+++ Experience/World/Controls.js
@@ -35,8 +35,8 @@ export default class Controls {
         this.firstMoveTimeline = new GSAP.timeline({
           scrollTrigger: {
             trigger: ".first-move",
-            top: "top top",
-            bottom: "bottom bottom",
+            start: "top top",
+            end: "bottom bottom",
             scrub: 0.6,
             invalidateOnRefresh: true
           }
@@ -51,8 +51,8 @@ export default class Controls {
         this.secondMoveTimeline = new GSAP.timeline({
           scrollTrigger: {
             trigger: ".second-move",
-            top: "top top",
-            bottom: "bottom bottom",
+            start: "top top",
+            end: "bottom bottom",
             scrub: 0.6,
             invalidateOnRefresh: true
           }
@@ -91,8 +91,8 @@ export default class Controls {
         this.thirdMoveTimeline = new GSAP.timeline({
           scrollTrigger: {
             trigger: ".third-move",
-            top: "top top",
-            bottom: "bottom bottom",
+            start: "top top",
+            end: "bottom bottom",
             scrub: 0.6,
             invalidateOnRefresh: true
           }
@@ -114,8 +114,8 @@ export default class Controls {
         this.firstMoveTimeline = new GSAP.timeline({
           scrollTrigger: {
             trigger: ".first-move",
-            top: "top top",
-            bottom: "bottom bottom",
+            start: "top top",
+            end: "bottom bottom",
             scrub: 0.6,
             invalidateOnRefresh: true
           }
@@ -128,8 +128,8 @@ export default class Controls {
         this.secondMoveTimeline = new GSAP.timeline({
           scrollTrigger: {
             trigger: ".second-move",
-            top: "top top",
-            bottom: "bottom bottom",
+            start: "top top",
+            end: "bottom bottom",
             scrub: 0.6,
             invalidateOnRefresh: true
           }
@@ -163,8 +163,8 @@ export default class Controls {
         this.thirdMoveTimeline = new GSAP.timeline({
           scrollTrigger: {
             trigger: ".third-move",
-            top: "top top",
-            bottom: "bottom bottom",
+            start: "top top",
+            end: "bottom bottom",
             scrub: 0.6,
             invalidateOnRefresh: true
           }
@@ -175,6 +175,64 @@ export default class Controls {
 
       // all
       all: () => {
+        // Progress Bar animation
+
+        this.sections = document.querySelectorAll(".section")
+        this.sections.forEach(section => {
+          this.progressWrapper = section.querySelector(".progress-wrapper")
+          this.progressBar = section.querySelector(".progress-bar")
+
+          if (section.classList.contains("right")) {
+            GSAP.to(section, {
+              borderTopLeftRadius: 10,
+              scrollTrigger: {
+                trigger: section,
+                start: "top bottom",
+                end: "top top",
+                scrub: 0.6
+              }
+            })
+            GSAP.to(section, {
+              borderBottomLeftRadius: 700,
+              scrollTrigger: {
+                trigger: section,
+                start: "bottom bottom",
+                end: "bottom top",
+                scrub: 0.6
+              }
+            })
+          } else {
+            GSAP.to(section, {
+              borderTopRightRadius: 10,
+              scrollTrigger: {
+                trigger: section,
+                start: "top bottom",
+                end: "top top",
+                scrub: 0.6
+              }
+            })
+            GSAP.to(section, {
+              borderBottomRightRadius: 700,
+              scrollTrigger: {
+                trigger: section,
+                start: "bottom bottom",
+                end: "bottom top",
+                scrub: 0.6
+              }
+            })
+          }
+          GSAP.from(this.progressBar, {
+            scaleY: 0,
+            scrollTrigger: {
+              trigger: section,
+              start: "top top",
+              end: "bottom bottom",
+              scrub: 0.4,
+              pin: this.progressWrapper,
+              pinSpacing: false
+            }
+          })
+        })
         // Mini Platform animation
         this.secondPartTimeline = new GSAP.timeline({
           scrollTrigger: {
