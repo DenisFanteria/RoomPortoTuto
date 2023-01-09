e1340fa9e2c8d68e2827aefdfaec3145f7493c42
DenisFanteria <67110836+DenisFanteria@users.noreply.github.com>
2023-01-09 19:59:41 +0100

    Preloader begining

---
+++ Experience/Preloader.js
@@ -0,0 +1,12 @@
+import { EventEmitter } from "events"
+
+export default class Preloader extends EventEmitter {
+  constructor() {
+    super()
+    this.experience = new Experience()
+    this.scene = this.experience.scene
+    this.sizes = this.experience.sizes
+    this.resources = this.experience.resources
+    this.camera = this.experience.camera
+  }
+}
