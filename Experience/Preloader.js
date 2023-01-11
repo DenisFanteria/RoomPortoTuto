4c63788f06c4d5f5f99b7e7a6dee532d6aee9b29
DenisFanteria <67110836+DenisFanteria@users.noreply.github.com>
2023-01-11 20:17:19 +0100

    More intro work

--- Experience/Preloader.js
+++ Experience/Preloader.js
@@ -69,83 +69,208 @@ export default class Preloader extends EventEmitter {
   secondIntro() {
     return new Promise(resolve => {
       this.secondTimeline = new GSAP.timeline()
-      if (this.device === "desktop") {
-        this.secondTimeline
-          .to(
-            this.room.position,
-            {
-              x: 0,
-              y: 0,
-              z: 0,
-              ease: "power1.out"
-            },
-            "same"
-          )
-          .to(
-            this.roomChildren.cube.rotation,
-            {
-              y: 2 * Math.PI + Math.PI / 4
-            },
-            "same"
-          )
-          .to(
-            this.roomChildren.cube.scale,
-            {
-              x: 10,
-              y: 10,
-              z: 10
-            },
-            "same"
-          )
-          .to(
-            this.camera.orthographicCamera.position,
-            {
-              y: 6.5
-            },
-            "same"
-          )
-          .to(
-            this.roomChildren.cube.position,
-            {
-              x: 0.638711,
-              y: 8.5618,
-              z: 1.3243
-            },
-            "same"
-          )
-          .to(this.roomChildren.body.scale, {
-            x: 1,
-            y: 1,
-            z: 1
-          })
-          .to(this.roomChildren.cube.scale, {
+      this.secondTimeline
+        .to(
+          this.room.position,
+          {
             x: 0,
             y: 0,
-            z: 0
-          })
-      } else {
-        this.secondTimeline.to(this.room.position, {
+            z: 0,
+            ease: "power1.out"
+          },
+          "same"
+        )
+        .to(
+          this.roomChildren.cube.rotation,
+          {
+            y: 2 * Math.PI + Math.PI / 4
+          },
+          "same"
+        )
+        .to(
+          this.roomChildren.cube.scale,
+          {
+            x: 10,
+            y: 10,
+            z: 10
+          },
+          "same"
+        )
+        .to(
+          this.camera.orthographicCamera.position,
+          {
+            y: 6.5
+          },
+          "same"
+        )
+        .to(
+          this.roomChildren.cube.position,
+          {
+            x: 0.638711,
+            y: 8.5618,
+            z: 1.3243
+          },
+          "same"
+        )
+        .to(this.roomChildren.body.scale, {
+          x: 1,
+          y: 1,
+          z: 1
+        })
+        .to(this.roomChildren.cube.scale, {
           x: 0,
           y: 0,
-          z: 0,
-          ease: "power1.out",
-          duration: 0.7
+          z: 0
         })
-      }
+        .to(
+          this.roomChildren.aquarium.scale,
+          {
+            x: 1,
+            y: 1,
+            z: 1,
+            ease: "back.out(2.2)",
+            duration: 0.5
+          },
+          ">-0.5"
+        )
+        .to(
+          this.roomChildren.clock.scale,
+          {
+            x: 1,
+            y: 1,
+            z: 1,
+            ease: "back.out(2.2)",
+            duration: 0.5
+          },
+          ">-0.4"
+        )
+        .to(
+          this.roomChildren.shelves.scale,
+          {
+            x: 1,
+            y: 1,
+            z: 1,
+            ease: "back.out(2.2)",
+            duration: 0.5
+          },
+          ">-0.3"
+        )
+        .to(
+          this.roomChildren.floor_items.scale,
+          {
+            x: 1,
+            y: 1,
+            z: 1,
+            ease: "back.out(2.2)",
+            duration: 0.5
+          },
+          ">-0.2"
+        )
+        .to(
+          this.roomChildren.desks.scale,
+          {
+            x: 1,
+            y: 1,
+            z: 1,
+            ease: "back.out(2.2)",
+            duration: 0.5
+          },
+          ">-0.1"
+        )
+        .to(
+          this.roomChildren.table_stuff.scale,
+          {
+            x: 1,
+            y: 1,
+            z: 1,
+            ease: "back.out(2.2)",
+            duration: 0.5
+          },
+          ">-0.1"
+        )
+        .to(this.roomChildren.computer.scale, {
+          x: 1,
+          y: 1,
+          z: 1,
+          ease: "back.out(2.2)",
+          duration: 0.5
+        })
+        .set(this.roomChildren.mini_floor.scale, {
+          x: 1,
+          y: 1,
+          z: 1
+        })
+        .to(
+          this.roomChildren.chair.scale,
+          {
+            x: 1,
+            y: 1,
+            z: 1,
+            ease: "back.out(2.2)",
+            duration: 0.5
+          },
+          "chair"
+        )
+        .to(
+          this.roomChildren.fish.scale,
+          {
+            x: 1,
+            y: 1,
+            z: 1,
+            ease: "back.out(2.2)",
+            duration: 0.5
+          },
+          "chair"
+        )
+        .to(
+          this.roomChildren.chair.rotation,
+          {
+            y: 4 * Math.PI + Math.PI / 4,
+            ease: "power2.out",
+            duration: 1,
+            onComplete: resolve
+          },
+          "chair"
+        )
     })
   }
 
   onScroll(e) {
     if (e.deltaY > 0) {
-      window.removeEventListener("wheel", this.scrollOnceEvent)
+      this.removeEventListeners()
       this.playSecondIntro()
     }
+  }
+
+  onTouch(e) {
+    this.initalY = e.touches[0].clientY
+  }
+
+  onTouchMove(e) {
+    let currentY = e.touches[0].clientY
+    let difference = this.initalY - currentY
+    if (difference > 0) {
+      console.log("swipe up")
+      this.removeEventListeners()
+      this.playSecondIntro()
+    }
+    this.initalY = null
+  }
+
+  removeEventListeners() {
+    window.removeEventListener("wheel", this.scrollOnceEvent)
+    window.removeEventListener("touchstart", this.touchStart)
+    window.removeEventListener("touchmove", this.touchMove)
   }
 
   async playIntro() {
     await this.firstIntro()
     this.scrollOnceEvent = this.onScroll.bind(this)
+    this.touchStart = this.onTouch.bind(this)
+    this.touchMove = this.onTouchMove.bind(this)
     window.addEventListener("wheel", this.scrollOnceEvent)
+    window.addEventListener("touchstart", this.touchStart)
+    window.addEventListener("touchmove", this.touchMove)
   }
 
   async playSecondIntro() {
